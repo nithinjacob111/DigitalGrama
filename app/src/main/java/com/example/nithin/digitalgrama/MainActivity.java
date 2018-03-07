2cd8b73aa9ebd2f6666a1f7d6107b5eb363053bd
nithinjacob111 <nithinjacob27gmail.com>
2018-03-07 11:19:29 +0530

    first

--- app/src/main/java/com/example/nithin/digitalgrama/MainActivity.java
+++ app/src/main/java/com/example/nithin/digitalgrama/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.nithin.digitalgrama;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window w = getWindow();
+            w.addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+            w.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        }
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                // TODO: Your application init goes here.
+                Intent mInHome = new Intent(MainActivity.this, HomeActivity.class);
+                MainActivity.this.startActivity(mInHome);
+                MainActivity.this.finish();
+            }
+        }, 3000);
     }
 }
